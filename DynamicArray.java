b730848f09d287c7ec5063ce4736bd3a216f6cf3
Prerana6675 <114150993+Prerana6675@users.noreply.github.com>
2022-09-23 18:42:38 +0530

    Add files via upload

---
+++ DynamicArray.java
@@ -0,0 +1,24 @@
+package com.yash.assign;
+
+public class DynamicArray {
+	
+		 
+	    
+	    public static void main(String[] args)
+	    {
+	 
+	       
+	    	Darray numbers = new Darray(3);
+	 
+	       
+	        numbers.insert(10);
+	        numbers.insert(30);
+	        numbers.insert(40);
+	        numbers.insert(50);
+	        numbers.insert(51);
+	 
+	       
+	        numbers.printArray();
+	    }
+	}
+
